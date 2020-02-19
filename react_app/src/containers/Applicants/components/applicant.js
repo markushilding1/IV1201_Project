1428f357a058734672bc0475cd749db43165f7fa
Philip Romin <rominphilip@gmail.com>
2020-02-19 19:20:20 +0100

    added pagination and sort

--- react_app/src/containers/Applicants/components/applicant.js
+++ react_app/src/containers/Applicants/components/applicant.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {Row} from 'react-bootstrap';
 
+<<<<<<< HEAD
 const applicant = ({name, apDate}) => {
+=======
+const applicant = ({ name, surname, apDate }) => {
+>>>>>>> added pagination and sort
   return (
     <Row className="applicants">
-      <p>{name}</p> <p>{apDate}</p>
+      <p>
+        {name} {surname}
+      </p>
+      <p>{apDate}</p>
     </Row>
   );
 };
