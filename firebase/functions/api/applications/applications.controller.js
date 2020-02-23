621e3ecc6c9768ef2961b740ddd9110f7956a045
Philip Romin <rominphilip@gmail.com>
2020-02-23 12:30:12 +0100

    Refactored view into multiple components

--- firebase/functions/api/applications/applications.controller.js
+++ firebase/functions/api/applications/applications.controller.js
@@ -2,49 +2,39 @@ const applicationsService = require("./applications.service");
 const { check, validationResult } = require("express-validator");
 
 //Controller to handle submission of an application
-<<<<<<< HEAD
 
 exports.submitApplication = async (req, res, next) => {
-
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        res.status(422).json({ errors: errors.array() });
-        return;
-    }
-
-    console.log(req.body);
-    console.log(req.body.uid);
-    const result = await applicationsService.submitApplication(req.body.areaOfExpertise,req.body.date,req.body.uid);
-    console.log(result)
-    if(result){
-        res.send(result);
-    }
-    else {
-        res.status(403);
-        res.send();
-    }
-};
-
-exports.getAreaOfExpertise = async (req,res,next)=> {
-    const result = await applicationsService.getAreaOfExpertise();
-
-    if(result){
-        res.send(result);
-    } else {
-        res.status(403);
-        res.send();
-    }
-=======
-exports.submitApplication = (req, res, next) => {
-  // Validating body params
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
+    res.status(422).json({ errors: errors.array() });
+    return;
   }
 
-  console.log("From Applications Controller");
-  return applicationsService.submitApplication();
->>>>>>> now fetching applicants from our database
+  console.log(req.body);
+  console.log(req.body.uid);
+  const result = await applicationsService.submitApplication(
+    req.body.areaOfExpertise,
+    req.body.date,
+    req.body.uid
+  );
+  console.log(result);
+  if (result) {
+    res.send(result);
+  } else {
+    res.status(403);
+    res.send();
+  }
+};
+
+exports.getAreaOfExpertise = async (req, res, next) => {
+  const result = await applicationsService.getAreaOfExpertise();
+
+  if (result) {
+    res.send(result);
+  } else {
+    res.status(403);
+    res.send();
+  }
 };
 
 /**
@@ -80,93 +70,45 @@ exports.getApplications = async (req, res, next) => {
  * @author Markus Hilding
  * @description Parameter validation for all controller functions where
  * params are sent via body.
-<<<<<<< HEAD
  * @param {string} method Name of controller method to validate.
- */
-exports.validate = (method) => {
-    switch (method) {
-        case 'getApplications': {
-            return [ 
-                check('name', 'Parameter ´name´ not provided').exists().isLength({min:2}),
-                check('competence', 'Parameter ´competence´ not provided').exists().isLength({min:2}),
-                check('fromDate', 'Parameter ´fromDate´ not provided').exists().isLength({min:10}),
-                check('toDate', 'Parameter ´toDate´ not provided').exists().isLength({min:10}),
-                check('page', 'Parameter ´page´ not provided').exists().isInt(),
-                check('dateOrder', 'Parameter ´dateOrder´ not provided').exists().isIn(['asc', 'desc']),
-
-            ]   
-        }
-
-        case 'submitApplication': {
-            return [
-                check('areaOfExpertise', 'Parameter ´areaOfExpertise´ not provided').exists(),
-                check('date', 'Parameter ´date´ not provided').exists(),
-                check('uid', 'Parameter ´uid´ not provided').exists(),
-            ]
-        }
-=======
- * @param {str} method Name of controller method to validate.
  */
 exports.validate = method => {
   switch (method) {
     case "getApplications": {
       return [
-        check("name", "Parameter ´name´ not provided"),
-        check("competence", "Parameter ´competence´ not provided").isLength({
-          min: 2
-        }),
-        check("fromDate", "Parameter ´fromDate´ not provided").isLength({
-          min: 10
-        }),
-        check("toDate", "Parameter ´toDate´ not provided").isLength({
-          min: 10
-        }),
+        check("name", "Parameter ´name´ not provided")
+          .exists()
+          .isLength({ min: 2 }),
+        check("competence", "Parameter ´competence´ not provided")
+          .exists()
+          .isLength({ min: 2 }),
+        check("fromDate", "Parameter ´fromDate´ not provided")
+          .exists()
+          .isLength({ min: 10 }),
+        check("toDate", "Parameter ´toDate´ not provided")
+          .exists()
+          .isLength({ min: 10 }),
         check("page", "Parameter ´page´ not provided")
           .exists()
           .isInt(),
-        check("sort", "Parameter ´dateOrder´ not provided").isIn([
-          "ASC",
-          "DESC"
-        ])
+        check("dateOrder", "Parameter ´dateOrder´ not provided")
+          .exists()
+          .isIn(["asc", "desc"])
       ];
     }
->>>>>>> now fetching applicants from our database
 
     case "submitApplication": {
       return [
         check(
           "areaOfExpertise",
-          "Parameter ´areaOfExpertis´ not provided"
+          "Parameter ´areaOfExpertise´ not provided"
         ).exists(),
-        check(
-          "areaOfExpertise.areaId",
-          "Parameter ´areaOfExpertis.areaId´ not provided"
-        )
-          .exists()
-          .isInt(),
-        check(
-          "areaOfExpertise.yearsOfExperience",
-          "Parameter ´areaOfExpertis.yearsOfExperience´ not provided"
-        ).exists(),
-        check(
-          "availabilityPeriod",
-          "Parameter ´availabilityPeriod´ not provided"
-        ).exists(),
-        check(
-          "availabilityPeriod.fromDate",
-          "Parameter ´availabilityPeriod´ not provided"
-        ).exists(),
-        check(
-          "availabilityPeriod.toDate",
-          "Parameter ´availabilityPeriod´ not provided"
-        ).exists()
+        check("date", "Parameter ´date´ not provided").exists(),
+        check("uid", "Parameter ´uid´ not provided").exists()
       ];
     }
-<<<<<<< HEAD
-=======
 
     default:
       return null;
   }
->>>>>>> now fetching applicants from our database
 };
--- react_app/src/containers/Applicants/view.js
+++ react_app/src/containers/Applicants/view.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Container, Row, Col, Form, Button, Spinner } from 'react-bootstrap';
-
-import 'react-dates/initialize';
-import { DateRangePicker } from 'react-dates';
-import 'react-dates/lib/css/_datepicker.css';
-import ReactPaginate from 'react-paginate';
+import { Container, Row, Col, Spinner } from 'react-bootstrap';
 
 import Applicant from './components/applicant';
+import SearchForm from './components/seachForm';
+import PageBar from './components/pageBar';
 import './style.css';
 
 /**
  * @description View functional component for applicants page
  * @author Philip Romn
  */
-const view = (props) => {
+const View = (props) => {
   const {
     applicants,
     loading,
@@ -26,61 +23,25 @@ const view = (props) => {
     onDatesChange,
     onFocusChange,
     handlePageClick,
-    error,
   } = props;
 
   if (!authorized) {
     return null;
   }
+
   return (
     <Container className="outerContainer">
       <Row className="searchBar">
         <Col>
-          <Form onChange={onFormChange}>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridNamn">
-                <Form.Label className="formLabel">Name</Form.Label>
-                <Form.Control name="name" />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridNamn">
-                <Form.Label className="formLabel">Availability</Form.Label>
-                <DateRangePicker
-                  startDate={startDate}
-                  startDateId="your_unique_start_date_id"
-                  endDate={endDate}
-                  endDateId="your_unique_end_date_id"
-                  onDatesChange={({ startDate, endDate }) =>
-                    onDatesChange({ startDate, endDate })
-                  }
-                  focusedInput={focusedInput}
-                  onFocusChange={(focusedInput) => onFocusChange(focusedInput)}
-                />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridCompetence">
-                <Form.Label className="formLabel">Competence</Form.Label>
-                <Form.Control name="competence" as="select">
-                  <option></option>
-                  <option>Korvgrillning</option>
-                  <option>Karuselldrift</option>
-                </Form.Control>
-              </Form.Group>
-              <Form.Group as={Col} controlId="formGridSort">
-                <Form.Label className="formLabel">
-                  Sort by application date
-                </Form.Label>
-                <Form.Control name="sort" as="select">
-                  <option></option>
-                  <option>ASC</option>
-                  <option>DESC</option>
-                </Form.Control>
-              </Form.Group>
-            </Form.Row>
-            <Button onClick={onSubmit} className="searchBtn">
-              Search
-            </Button>
-          </Form>
+          <SearchForm
+            onFormChange={onFormChange}
+            onSubmit={onSubmit}
+            startDate={startDate}
+            endDate={endDate}
+            focusedInput={focusedInput}
+            onDatesChange={onDatesChange}
+            onFocusChange={onFocusChange}
+          />
         </Col>
       </Row>
       <Container className="innerContainer">
@@ -96,29 +57,9 @@ const view = (props) => {
           ))
         )}
       </Container>
-      <ReactPaginate
-        previousLabel={'previous'}
-        nextLabel={'next'}
-        breakLabel={'...'}
-        pageCount={10}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={5}
-        onPageChange={handlePageClick}
-        subContainerClassName={'pages pagination'}
-        activeClassName={'active'}
-        breakClassName={'page-item'}
-        breakLinkClassName={'page-link'}
-        containerClassName={'pagination'}
-        pageClassName={'page-item'}
-        pageLinkClassName={'page-link'}
-        previousClassName={'page-item'}
-        previousLinkClassName={'page-link'}
-        nextClassName={'page-item'}
-        nextLinkClassName={'page-link'}
-        activeClassName={'active'}
-      />
+      <PageBar handlePageClick={handlePageClick} />
     </Container>
   );
 };
 
-export default view;
+export default View;
